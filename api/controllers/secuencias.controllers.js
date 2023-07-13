cba5c399d796b7d32c59d290603afdf8471922a0
Joel Concepción Villanueva <joel.concepcion@gmail.com>
2023-07-13 17:04:00 +0100

    Secuencias controllers updatePictoosInSecuencia is done

--- api/controllers/secuencias.controllers.js
+++ api/controllers/secuencias.controllers.js
@@ -1,3 +1,4 @@
+const Imagenes = require('../models/imagenes.models.js')
 const Secuencias = require('../models/secuencias.models.js')
 
 async function getAllSecuencias(req, res){
@@ -49,7 +50,7 @@ async function createSecuencia(req, res){
         const secuencia = await Secuencias.create(req.body)
 
         if (secuencia) {
-            return res.status(200). json(secuencia)
+            return res.status(200).json(secuencia)
         }else{
             res.status(404).send(error.message)
         }
@@ -70,4 +71,65 @@ async function updateDataSecuencia(req, res){
     }
 }
 
-module.exports = {getAllSecuencias, getOneSecuencia, deleteSecuencia, createSecuencia, updateDataSecuencia}
+async function getAllPictoBySecuencia(req, res){
+    /* console.log(req.params.id)
+    res.status(200).send(req.params.id) */
+    try {
+        const secuencia = await Secuencias.findByPk(req.params.id)
+        const pictos = await secuencia.getImagens()
+        if (pictos) {
+            return res.status(200).json(pictos)
+        }else{
+            res.status(404).send('Pictos not found')
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+async function updatePictosInSecuencia(req, res){
+    try {
+        const secuencia = await Secuencias.findByPk(req.params.id)
+        const array = []
+        const arrayBody = req.body.array
+        for(let i = 0; i < arrayBody.length; i++) {
+            const imagen = await Imagenes.findByPk(req.body.array[i])
+            array.push(imagen)
+        }  
+        secuencia.setImagens(array)
+        res.status(200).json(array)
+    } catch (error) {
+        
+    }
+}
+
+
+async function getPictoBySecuencia(req, res){
+    try {
+        const secuencia = await Secuencias.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: Imagenes
+        })
+        const length = secuencia.imagens.length
+        const array = []
+        for(let i = 0; i < length; i++) {
+            array.push(secuencia.imagens[i].id)
+        } 
+        console.log(array)
+        if (secuencia) {
+            return res.status(200).json(secuencia)
+        }else{
+            res.status(404).send('Pictos not found')
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+
+
+module.exports = {getAllSecuencias, getOneSecuencia, deleteSecuencia, createSecuencia, updateDataSecuencia, getAllPictoBySecuencia, updatePictosInSecuencia, getPictoBySecuencia}
--- api/routes/secuencias.route.js
+++ api/routes/secuencias.route.js
@@ -1,15 +1,19 @@
 const router = require('express').Router()
 
-const {getAllSecuencias, getOneSecuencia, deleteSecuencia, createSecuencia, updateDataSecuencia} = require('../controllers/secuencias.controllers')
+const {getAllSecuencias, getOneSecuencia, deleteSecuencia, createSecuencia, updateDataSecuencia, getAllPictoBySecuencia, updatePictosInSecuencia, getPictoBySecuencia} = require('../controllers/secuencias.controllers')
 
 router.get('/', getAllSecuencias)
 
 router.get('/:id', getOneSecuencia)
+
+router.get('/:id/pictos', getAllPictoBySecuencia)
 
 router.delete('/:id', deleteSecuencia)
 
 router.post('/', createSecuencia)
 
 router.put('/:id', updateDataSecuencia)
+
+router.put('/:id/update', updatePictosInSecuencia)
 
 module.exports = router
